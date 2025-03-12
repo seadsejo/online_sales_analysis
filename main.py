0e6fab8753c28954f4c8ea3ad6bd6b6f996986db
Sead <ljubovicsead02@gmail.com>
2025-03-12 21:16:19 +0100

    Adding main.py to online sales analysis

---
+++ main.py
@@ -0,0 +1,16 @@
+from product_manager import ProductManager
+from product import Product
+
+
+manager = ProductManager()
+
+manager.add_product(Product("PC", 1400.00, 7))
+manager.add_product(Product("Laptop", 1020.60, 10))
+manager.add_product(Product("Headphones", 24.99, 15))
+manager.add_product(Product("4K Monitor", 999.99, 9))
+
+print("Displaying available products: ")
+manager.display_all_products()
+
+total = manager.total_inventory_value()
+print(f"Total inventory value is: {total:.2f}$")
