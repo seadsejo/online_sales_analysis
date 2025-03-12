268a4019606b9e852fc63cacf3205b418c2b2f26
Sead <ljubovicsead02@gmail.com>
2025-03-12 23:33:35 +0100

    Changed quantity and removed some lines of code

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ from product import Product
 
 manager = ProductManager()
 
-manager.add_product(Product("PC", 1400.00, 7))
-manager.add_product(Product("Laptop", 1020.60, 10))
-manager.add_product(Product("Headphones", 24.99, 15))
-manager.add_product(Product("4K Monitor", 999.99, 9))
+manager.add_product(Product("PC", 1400.00, 12))
+manager.add_product(Product("Laptop", 1020.60, 9))
+manager.add_product(Product("Headphones", 24.99, 17))
+manager.add_product(Product("4K Monitor", 999.99, 20))
 
-print("Displaying available products: ")
-manager.display_all_products()
+#print("Displaying available products: ")
+#manager.display_all_products()
 
-total = manager.total_inventory_value()
-print(f"\nTotal inventory value is: {total:.2f}$\n")
+#total = manager.total_inventory_value()
+#print(f"\nTotal inventory value is: {total:.2f}$\n")
 
 print(manager.product_removal("Headphones"))
-
-print("\nDisplaying available products: ")
-manager.display_all_products()
