3a007e371c9995e32b91265d1e8299eb012b40d5
Sead <ljubovicsead02@gmail.com>
2025-03-12 22:13:22 +0100

    Added method product_removal to product_manager.py

--- main.py
+++ main.py
@@ -13,4 +13,9 @@ print("Displaying available products: ")
 manager.display_all_products()
 
 total = manager.total_inventory_value()
-print(f"Total inventory value is: {total:.2f}$")
+print(f"\nTotal inventory value is: {total:.2f}$\n")
+
+print(manager.product_removal("Headphones"))
+
+print("\nDisplaying available products: ")
+manager.display_all_products()
--- product_manager.py
+++ product_manager.py
@@ -21,3 +21,12 @@ class ProductManager:
     def total_inventory_value(self):
         total_value = sum(product.price * product.quantity for product in self.products)
         return total_value
+    
+
+    def product_removal(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                self.products.remove(product)
+                return f"Product '{product_name}' has been removed"
+
+        return f"Product '{product_name}' is not available"
